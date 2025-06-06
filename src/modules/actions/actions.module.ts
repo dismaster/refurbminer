7756f556eb3995fa8c21cb11f1b3d10f16db0b02
Rene <rene.golz84@gmail.com>
2025-06-06 13:05:17 +0200

    Update actions.module.ts

--- src/modules/actions/actions.module.ts
+++ src/modules/actions/actions.module.ts
@@ -5,16 +5,18 @@ import { ConfigModule } from '../config/config.module';
 import { LoggingModule } from '../logging/logging.module';
 import { ApiCommunicationModule } from '../api-communication/api-communication.module';
 import { MinerManagerModule } from '../miner-manager/miner-manager.module';
+import { OsDetectionModule } from '../device-monitoring/os-detection/os-detection.module';
 
 @Module({
   imports: [
     ConfigModule,
     LoggingModule,
     ApiCommunicationModule,
-    MinerManagerModule
+    MinerManagerModule,
+    OsDetectionModule,
   ],
   providers: [ActionsService],
   controllers: [ActionsController],
-  exports: [ActionsService]
+  exports: [ActionsService],
 })
 export class ActionsModule {}
