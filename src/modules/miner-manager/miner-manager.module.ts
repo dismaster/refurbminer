23cc63347cb7e310548c3d838a02451820f9a324
Rene <rene.golz84@gmail.com>
2025-06-07 16:21:39 +0200

    Update miner-manager.module.ts

--- src/modules/miner-manager/miner-manager.module.ts
+++ src/modules/miner-manager/miner-manager.module.ts
@@ -4,10 +4,17 @@ import { MinerManagerController } from './miner-manager.controller';
 import { LoggingModule } from '../logging/logging.module';
 import { ApiCommunicationModule } from '../api-communication/api-communication.module';
 import { FlightsheetModule } from '../flightsheet/flightsheet.module';
-import { ConfigModule } from '../config/config.module'; // Import ConfigModule
+import { ConfigModule } from '../config/config.module';
+import { MinerSoftwareModule } from '../miner-software/miner-software.module';
 
 @Module({
-  imports: [LoggingModule, ApiCommunicationModule, FlightsheetModule, ConfigModule],
+  imports: [
+    LoggingModule,
+    ApiCommunicationModule,
+    FlightsheetModule,
+    ConfigModule,
+    MinerSoftwareModule,
+  ],
   providers: [MinerManagerService],
   controllers: [MinerManagerController],
   exports: [MinerManagerService],
